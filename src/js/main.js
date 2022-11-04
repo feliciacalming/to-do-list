43bfb1029d06e89601cb34a28720aa1adadebaf9
Felicia Calming <felicia.calming@gmail.com>
2022-11-04 15:32:52 +0100

    added scss for desktop version

--- src/js/main.js
+++ src/js/main.js
@@ -5,16 +5,23 @@ const userInput = document.getElementById("user-input");
 const inputContainer = document.getElementById("input-container");
 const inputButton = document.createElement("button");
 const listContainer = document.getElementById("uncompleted-tasks"); // måste ligga utanför loopen, annars blir det två div:ar
-const sortButton = document.getElementById("sort-list");
+const sortButton = document.getElementById("sort-btn");
+const list = document.createElement("ul");
+const clearButton = document.getElementById("clear-btn");
 
 //klassnamn
 inputButton.classList.add("input__button");
 listContainer.classList.add("list-container");
 
 //knappar
-inputButton.innerHTML = "lägg till to do!";
+inputButton.innerHTML = "+";
 inputButton.addEventListener("click", addTodo);
 sortButton.addEventListener("click", sortAlphabetical);
+clearButton.addEventListener("click", () => {
+  localStorage.clear();
+  list.innerHTML = "";
+  localStorage.setItem("to-do", JSON.stringify(toDoList));
+});
 
 // class ToDo {
 //   constructor(task, completed) {
@@ -24,9 +31,9 @@ sortButton.addEventListener("click", sortAlphabetical);
 // }
 
 let toDoList = [
-  new ToDo("gå till systemet", false),
-  new ToDo("köp pizza", false),
-  new ToDo("måla naglarna", false),
+  new ToDo("Gå till systemet", false),
+  new ToDo("Köp pizza", false),
+  new ToDo("Måla naglarna", false),
 ];
 
 //startar funktionen så att min hårdkodade lista visas direkt
@@ -61,28 +68,33 @@ function addTodo() {
 
 //funktion som skapar en synlig lista för mina
 function createToDoList() {
-  listContainer.innerHTML = "";
+  // listContainer.innerHTML = "";
+  list.innerHTML = "";
   for (let i = 0; i < toDoList.length; i++) {
-    const list = document.createElement("ul");
+    //PROVAR ÄNDRA OM
+    const itemContainer = document.createElement("span");
     const task = document.createElement("li");
     const checkbox = document.createElement("input");
     let deleteButton = document.createElement("button");
 
     list.classList.add("list");
+    itemContainer.classList.add("list__item__container");
     task.classList.add("list__item");
     checkbox.classList.add("list__checkbox");
     deleteButton.classList.add("list__deletebutton");
 
     task.innerHTML = toDoList[i].task; // funkar inte att skriva + checkbox, måste göra myTask.appendChild(checkbox)!
 
-    //eventlyssnare för att hantera ifall checkboxen är iklickad blir boolean completed = true och tvärtom
+    //eventlyssnare för att hantera när checkboxen är iklickad
     checkbox.type = "checkbox";
     checkbox.checked = toDoList[i].completed;
     checkbox.addEventListener("click", () => {
       if (checkbox.checked === true) {
         toDoList[i].completed = true;
+        //försöker få de klara att hamna längst ner i listan
+
         task.classList.add("list__item--completed");
-        task.appendChild(checkbox);
+        // task.appendChild(checkbox);
       } else {
         toDoList[i].completed = false;
         task.classList.remove("list__item--completed");
@@ -106,10 +118,24 @@ function createToDoList() {
     //   console.log("Du klickade på", toDoList[i]);
     // }
 
+    //det här fick soptunnan att ligga längst bort me nförlorade funktionen delete
+    // list.appendChild(itemContainer);
+    // itemContainer.appendChild(task);
+    // task.appendChild(checkbox);
+    // itemContainer.appendChild(deleteButton);
+    // listContainer.appendChild(itemContainer);
+
+    // SENASTE ÄNDRINGEN:
     task.appendChild(checkbox);
     task.appendChild(deleteButton);
     list.appendChild(task);
     listContainer.append(list);
+
+    //sparar mitt gamla:
+    // task.appendChild(checkbox);
+    // task.appendChild(deleteButton);
+    // list.appendChild(task);
+    // listContainer.append(list);
   }
 }
 
